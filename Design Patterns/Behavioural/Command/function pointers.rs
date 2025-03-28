0fc8852cc97bac170e44dceaa2dfb45709cd285c
wdlin233 <wdlin233@163.com>
2025-03-28 22:06:41 +0800

    ✨ Add Command Pattern

---
+++ Design Patterns/Behavioural/Command/function pointers.rs
@@ -0,0 +1,46 @@
+/// creating each individual command as a different function 
+/// and store function pointers to invoke these functions later at a different time.
+type FnPtr = fn() -> String;
+struct Command {
+    execute: FnPtr,
+    rollback: FnPtr,
+}
+
+struct Schema {
+    commands: Vec<Command>,
+}
+
+impl Schema {
+    fn new() -> Self {
+        Self { commands: vec![] }
+    }
+    fn add_migration(&mut self, execute: FnPtr, rollback: FnPtr) {
+        self.commands.push(Command { execute, rollback });
+    }
+    fn execute(&self) -> Vec<String> {
+        self.commands.iter().map(|cmd| (cmd.execute)()).collect()
+    }
+    fn rollback(&self) -> Vec<String> {
+        self.commands
+            .iter()
+            .rev()
+            .map(|cmd| (cmd.rollback)())
+            .collect()
+    }
+}
+
+fn add_field() -> String {
+    "add field".to_string()
+}
+
+fn remove_field() -> String {
+    "remove field".to_string()
+}
+
+fn main() {
+    let mut schema = Schema::new();
+    schema.add_migration(|| "create table".to_string(), || "drop table".to_string());
+    schema.add_migration(add_field, remove_field);
+    assert_eq!(vec!["create table", "add field"], schema.execute());
+    assert_eq!(vec!["remove field", "drop table"], schema.rollback());
+}
---
+++ Design Patterns/Behavioural/Command/trait objects.rs
@@ -0,0 +1,66 @@
+/// Suppose we have a sequence of actions or transactions encapsulated as objects. 
+/// We want these actions or commands to be executed or invoked in some order later at different time. 
+
+/// The basic idea of the Command pattern is to separate out 
+/// actions into its own objects and pass them as parameters.
+pub trait Migration {
+    fn execute(&self) -> &str;
+    fn rollback(&self) -> &str;
+}
+
+pub struct CreateTable;
+impl Migration for CreateTable {
+    fn execute(&self) -> &str {
+        "create table"
+    }
+    fn rollback(&self) -> &str {
+        "drop table"
+    }
+}
+
+pub struct AddField;
+impl Migration for AddField {
+    fn execute(&self) -> &str {
+        "add field"
+    }
+    fn rollback(&self) -> &str {
+        "remove field"
+    }
+}
+
+struct Schema {
+    commands: Vec<Box<dyn Migration>>,
+}
+
+impl Schema {
+    fn new() -> Self {
+        Self { commands: vec![] }
+    }
+
+    fn add_migration(&mut self, cmd: Box<dyn Migration>) {
+        self.commands.push(cmd);
+    }
+
+    fn execute(&self) -> Vec<&str> {
+        self.commands.iter().map(|cmd| cmd.execute()).collect()
+    }
+    fn rollback(&self) -> Vec<&str> {
+        self.commands
+            .iter()
+            .rev() // reverse iterator's direction
+            .map(|cmd| cmd.rollback())
+            .collect()
+    }
+}
+
+fn main() {
+    let mut schema = Schema::new();
+
+    let cmd = Box::new(CreateTable);
+    schema.add_migration(cmd);
+    let cmd = Box::new(AddField);
+    schema.add_migration(cmd);
+
+    assert_eq!(vec!["create table", "add field"], schema.execute());
+    assert_eq!(vec!["remove field", "drop table"], schema.rollback());
+}
