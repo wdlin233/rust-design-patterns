8c73da6a71a260f1faeda0f92493529c0678322e
wdlin233 <wdlin233@163.com>
2025-03-29 22:38:36 +0800

    ✨ (feat) Add Newtype

---
+++ Design Patterns/Behavioural/Newtype.rs
@@ -0,0 +1,19 @@
+use std::fmt::Display;
+
+// Create Newtype Password to override the Display trait for String
+struct Password(String);
+
+impl Display for Password {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "****************")
+    }
+}
+
+/// zero-cost Abstraction
+/// It allows you to share implementation details between types while precisely controlling the interface.
+fn main() {
+    let unsecured_password: String = "ThisIsMyPassword".to_string();
+    let secured_password: Password = Password(unsecured_password.clone());
+    println!("unsecured_password: {unsecured_password}");
+    println!("secured_password: {secured_password}");
+}
