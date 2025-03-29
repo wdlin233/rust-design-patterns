6dd7ce88ad10968c4753a0d9ee91896e061ed95e
wdlin233 <wdlin233@163.com>
2025-03-29 22:35:08 +0800

    ✨ (feat) Add Interpreter Pattern

---
+++ Design Patterns/Behavioural/Interpreter/Edulidean length.rs
@@ -0,0 +1,26 @@
+///  In fact, this pattern is about expressing problem instances in a more 
+/// specific way and implementing functions/classes/structs that solve these problem instances
+
+/// Writing norm!(x,1,2) might be easier to express and more efficient than 
+/// packing x,1,2 into a Vec and calling a function computing the length.
+macro_rules! norm {
+    ($($element:expr),*) => {
+        {
+            let mut n = 0.0;
+            $(
+                n += ($element as f64)*($element as f64);
+            )*
+            n.sqrt()
+        }
+    };
+}
+
+fn main() {
+    let x = -3f64;
+    let y = 4f64;
+
+    assert_eq!(3f64, norm!(x));
+    assert_eq!(5f64, norm!(x, y));
+    assert_eq!(0f64, norm!(0, 0, 0));
+    assert_eq!(1f64, norm!(0.5, -0.5, 0.5, -0.5));
+}
